bb1ab7eecbf3e1ef585a4ae5857bd19b5afceb23
Simon Hadjadj <hadjadjsimon@gmail.com>
2023-01-27 12:22:43 +0100

    position potion et gold

--- Simon.py
+++ Simon.py
@@ -15,3 +15,21 @@ class Escalier :
         self.move = False 
         self.through = True 
         self.gettable = False 
+
+import random as rd
+def fonction(etage):
+    position_potion = ()
+    position_gold = ()
+    m, n = len(etage), len(etage[0])
+    while position_potion == () :
+        a, b= rd.randint(n), rd.randint(m)
+        if type(etage[a][b]) is Sol :
+            position_potion = (a, b)
+    while position_gold == () :
+        a, b = rd.randint(n), rd.randint(m)
+        if type(etage[a][b]) is Sol :
+            position_gold = (a, b)
+    return position_gold, position_potion
+        
+
+

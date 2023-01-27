1e3db3a38ac415489f61d9a7d2ad75b2524d1477
Simon Hadjadj <hadjadjsimon@gmail.com>
2023-01-27 10:17:25 +0100

    porte escalier et rideaux

---
+++ Simon.py
@@ -0,0 +1,12 @@
+class Porte : 
+    def __init__(self, x, y): 
+        def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.color = 'pink'
+
+class Escalier : 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.color = 'blue'

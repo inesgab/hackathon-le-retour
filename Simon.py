1e99002cb8750423b18d249134874ec6a4332041
Simon Hadjadj <hadjadjsimon@gmail.com>
2023-01-27 12:06:06 +0100

    david gettable

--- Simon.py
+++ Simon.py
@@ -5,6 +5,7 @@ class Porte :
         self.color = 'PINK'
         self.move = False
         self.through = True
+        self.gettable = False 
 
 class Escalier : 
     def __init__(self, x, y):
@@ -13,3 +14,4 @@ class Escalier :
         self.color = 'GREEN'
         self.move = False 
         self.through = True 
+        self.gettable = False 

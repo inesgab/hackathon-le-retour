6e0f35d4942750d528034b28b6cc5a6d5fbaa4b9
AntoineBPSL <abidault02@gmail.com>
2023-01-27 11:08:05 +0100

    première visu SUUUUUH

--- antoine.py
+++ antoine.py
@@ -3,6 +3,10 @@ from Simon import *
 from clemclem import *
 import pygame as pg
 
+LONGUEUR = 700
+LARGEUR = 500
+FD = 20
+
 CLASSES = {
     "#" : Couloir,
     "|" : Mur,
@@ -23,9 +27,32 @@ def convert_text2lab(fichier: str) -> list:
             for indice_colonne, char in enumerate(ligne_propre):
                 ligne_objet.append(CLASSES[char](indice_colonne, indice_ligne))
             etage.append(ligne_objet)
-
     return etage
 
+etage = convert_text2lab('premier_etage.txt')
 
+pg.init()
+screen = pg.display.set_mode((LONGUEUR, LARGEUR))
+clock = pg.time.Clock()
 
+running = True
 
+while running:
+
+    clock.tick(10)
+
+    rect = pg.Rect(0, 0, LONGUEUR, LARGEUR)
+    pg.draw.rect(screen, (255, 255, 255), rect)
+
+    for ligne in etage:
+        for objet in ligne:
+            rect = pg.Rect(FD*objet.x, FD*objet.y, FD, FD)
+            pg.draw.rect(screen, objet.color, rect)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    pg.display.update()
+
+pg.quit()

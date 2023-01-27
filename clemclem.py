db99605c7cf7afb293c46745bececc75115c8121
Clemence Jung <clemence.jung@etu.minesparis.psl.eu>
2023-01-27 11:44:50 +0100

    through

--- clemclem.py
+++ clemclem.py
@@ -4,12 +4,24 @@ class Sol:
         self.y = y
         self.color = 'white'
         self.move = False
+        self.through = True
 
 class Vide:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.color = 'black'
-        self.move = False       
+        self.move = False 
+        self.through = False
 
 
+
+
+class Potion:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y 
+        self.move = False
+
+class Potion_soin(Potion):
+    

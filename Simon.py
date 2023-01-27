7d4b816e623cc66bee3d5b1f4189768a65614aa2
Simon Hadjadj <hadjadjsimon@gmail.com>
2023-01-27 11:22:18 +0100

    move false

--- Simon.py
+++ Simon.py
@@ -3,9 +3,11 @@ class Porte :
         self.x = x
         self.y = y
         self.color = 'PINK'
+        self.move = False
 
 class Escalier : 
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.color = 'GREEN'
+        self.move

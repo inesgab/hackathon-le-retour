00702ac80cbe12c36f4ad62b0a0af2da21aeb3a7
Inès Gabert <ines.gabert@etu.minesparis.psl.eu>
2023-01-27 10:18:14 +0100

    nv classes

--- ines.py
+++ ines.py
@@ -6,17 +6,22 @@ class Couloir:
         self.color = 'gray'
     
 class Mur:
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.color = 'brown'
 
 class Perso:
-    def __init__(self, name, x, y, gold = 0, armor = 5, hits = 12, ):
+
+    def __init__(self, x, y, name = 'Luc', gold = 0, armor = 5, hits = 12):
         self.name = name
         self.x = x
         self.y = y
         self.gold = gold
         self.armor = armor
         self.hits = hits
+        self.color = 'red'
+
 
         
